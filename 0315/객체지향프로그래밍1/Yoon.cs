47add5b778735a7c53257e1ebf96fce281b12f28
parkjiho122 <96416429+parkjiho122@users.noreply.github.com>
2022-03-15 22:00:00 +0900

    0315

---
+++ 0315/객체지향프로그래밍1/Yoon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        bool whatis = true;
+        while (whatis)
+        {
+            int year = int.Parse(Console.ReadLine());
+            if (year < 1 || year > 3000)
+            {
+                Console.WriteLine("다시입력해주세요.");
+                continue;
+            }
+
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) Console.WriteLine(year+ "년은 윤년입니다.");
+            else Console.WriteLine(year+"년은 평년입니다.");
+        }
+    }
+}
