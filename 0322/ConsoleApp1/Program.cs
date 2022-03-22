d317d3e47a400c49e5059f8fc4571a6a12ea548c
parkjiho122 <96416429+parkjiho122@users.noreply.github.com>
2022-03-22 22:00:00 +0900

    0322

---
+++ 0322/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ParameterArrayApp
+{
+    class Program
+    {
+        static void SumAvg(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++) sum = sum + arr[i];
+
+            Console.WriteLine("sum : " + sum);
+            Console.WriteLine("avg : " + sum / arr.Length);
+        }
+        static void Main(string[] args)
+        {
+            int[] abc = { 1, 2, 3, 4, 5 };
+            SumAvg(abc);
+        }
+    }
+}
