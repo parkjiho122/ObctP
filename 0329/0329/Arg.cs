2d248fbcd5f9230478ee468592dade46e6e3d815
parkjiho122 <96416429+parkjiho122@users.noreply.github.com>
2022-03-29 22:00:00 +0900

    0329

---
+++ 0329/0329/Arg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+namespace Arg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; ++i)
+                Console.WriteLine("Argument[{0}] = {1}", i, args[i]);
+        }
+    }
+}
---
+++ 0329/0329/Sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    class Program
+    {
+        static void SumAvg(int[] arr)
+        {
+            int sum = 0;
+            foreach (var num in arr)
+            {
+                sum += num;
+            }
+            Console.WriteLine("sum : " + sum);
+            Console.WriteLine("avg : " + sum / arr.Length);
+        }
+        static void Main(string[] args)
+        {
+            int[] abc = new int[5] { 1, 2, 3, 4, 5 };
+            SumAvg(abc);
+        }
+    }
+}
