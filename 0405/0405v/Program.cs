675a39e64344d539124b3fcb5aabc5e5f813ee7c
parkjiho122 <96416429+parkjiho122@users.noreply.github.com>
2022-04-05 22:00:00 +0900

    0405

---
+++ 0405/0405v/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Program
+{
+    class School
+    {
+        public string name; 
+        public School(){
+            Console.WriteLine("부모 클래스");
+        }
+    }
+    class Students : School
+    {
+        public Students(){
+            Console.WriteLine("자식 클래스");
+        }
+        public void printStudents(string name)
+        {
+            this.name = name;
+            Console.WriteLine($"학생의 이름은 {name} 입니다.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students students = new Students();
+            students.printStudents("Park");
+
+        }
+    }
+}
